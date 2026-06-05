f4d047e9e0910c739c30c3d33cbd857a1efae457
kristina-99 <kristina_atan@abv.bg>
2026-06-05 13:14:01 +0300

    * added ArcadeMap scene to BootSceneDriver script * removed camera from the ArcadeMap scene as it conflicts with the main camera when loaded

--- Assets/Scripts/BootSceneDriver.cs
+++ Assets/Scripts/BootSceneDriver.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class BootSceneDriver : MonoBehaviour
 {
     private const string HUDscene = "Assets/Scenes/HUD.unity";
+    private const string ArcadeMap = "Assets/Scenes/Arcade_Map.unity";
     
     void Awake()
     {
@@ -19,13 +20,17 @@ public class BootSceneDriver : MonoBehaviour
     private IEnumerator LoadAdditionalScenes()
     {
         AsyncOperation loadHUD = SceneManager.LoadSceneAsync(HUDscene, LoadSceneMode.Additive);
+        yield return StartCoroutine(SingleSceneLoad(loadHUD)); 
+        AsyncOperation loadArcadeMap = SceneManager.LoadSceneAsync(ArcadeMap, LoadSceneMode.Additive);
+        yield return StartCoroutine(SingleSceneLoad(loadArcadeMap));
+        Destroy(gameObject);
+    }
 
-        while (!loadHUD.isDone)
+    private IEnumerator SingleSceneLoad(AsyncOperation loadScene)
+    {
+        while(!loadScene.isDone)
         {
             yield return null; 
         }
-
-        Destroy(gameObject);
     }
-    
 }
