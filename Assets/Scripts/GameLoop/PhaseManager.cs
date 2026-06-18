d247daf56a611ae6c9ce3313d6c4bcb4b7c99eea
kristina-99 <kristina_atan@abv.bg>
2026-06-18 15:49:59 +0300

    * implemented coroutine in the PhaseManager

--- Assets/Scripts/GameLoop/PhaseManager.cs
+++ Assets/Scripts/GameLoop/PhaseManager.cs
@@ -1,11 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using static GameConstants;
 
 public class PhaseManager : MonoBehaviour
 {
+    private const float NightDuration = 100f;
+    private  const float DayDuration = 120f;
     private float counter = 0f;
+    private int roundCounter = 0;
+    private Coroutine gameLoopRoutine;
+    private bool interruptNightPhase = false;
+    private bool gameOver = false;
+
+    void OnEnable()
+    {
+        GameEvents.OnBodyReported += InterruptNightPhase;
+        GameEvents.OnGameEnded += GameOver;
+    }
+
+    void OnDisable()
+    {
+        GameEvents.OnBodyReported -= InterruptNightPhase;
+        GameEvents.OnGameEnded -= GameOver;       
+    }
+
     void Start()
     {
-
+        gameLoopRoutine = StartCoroutine(GameLoopRoutine());
     }
 
     void Update()
@@ -15,5 +36,44 @@ public class PhaseManager : MonoBehaviour
         {
             RoleFactory.AssignRoles();
         }
+
     }
+
+    IEnumerator GameLoopRoutine()
+    {
+        while(!gameOver)
+        {
+            roundCounter++;
+            interruptNightPhase = false; 
+            GameEvents.NightStarted(roundCounter);
+
+            float nightEndTime = Time.time + NightDuration;
+            yield return new WaitUntil(() => Time.time >= nightEndTime || interruptNightPhase);
+
+            if (gameOver) break; 
+
+            GameEvents.DayStarted();
+            yield return new WaitForSeconds(DayDuration);
+        }
+        
+        Debug.Log("Game Over!");
+        gameLoopRoutine = null;
+    }
+
+    private void InterruptNightPhase(BaseCharacter reportedBody)
+    {
+        interruptNightPhase = true;
+    }
+
+    private void GameOver(GameResult gameResult)
+    {
+        gameOver = true;
+
+        if (gameLoopRoutine != null)
+        {
+            StopCoroutine(gameLoopRoutine);
+            gameLoopRoutine = null;
+        }
+    }
+
 }
