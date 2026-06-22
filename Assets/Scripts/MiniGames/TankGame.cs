d24198c6fc49d21064d6bce11ce951ef83be9986
kristina-99 <kristina_atan@abv.bg>
2026-06-22 15:52:31 +0300

    * made a MiniGameCabinet class attached to all cabinets * MiniGameCabinet acts as script for interacting with the cabinets and handling data for the minigames * emptied out separate minigame classes as their logic was the same and this logic is now contained in the MiniGameCabinet

--- Assets/Scripts/MiniGames/TankGame.cs
+++ Assets/Scripts/MiniGames/TankGame.cs
@@ -1,49 +1,6 @@
 using UnityEngine;
-using static GameConstants;
 
-public class TankGame : MonoBehaviour, IMiniGame
+public class TankGame : MonoBehaviour
 {
-    public CabinetConfigSO cabinetConfig;
-    private MiniGameType miniGameType;
-    private string displayGame;
-    private float taskDuration;
-    private RoomId assignedRoom;
-
-    public MiniGameType MiniGameType 
-    { 
-        get
-        {
-            return miniGameType;
-        }
-    }
-    public string DisplayName 
-    { 
-        get
-        {
-            return displayGame;
-        }
-    }
-    public float TaskDuration 
-    {
-        get
-        {
-            return taskDuration;
-        }
-    }
-    public RoomId AssignedRoom 
-    {
-        get
-        {
-            return assignedRoom;
-        }
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        miniGameType = cabinetConfig.miniGameType;
-        displayGame = cabinetConfig.displayName;
-        taskDuration = cabinetConfig.taskDuration;
-        assignedRoom = cabinetConfig.assignedRoom;
-    }
+    
 }
