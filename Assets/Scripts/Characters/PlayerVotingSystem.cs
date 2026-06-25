9e5d9587805a03add2bcba9b4ab6cf0f2cd1eba4
kristina-99 <kristina_atan@abv.bg>
2026-06-25 16:27:00 +0300

    * added buttons and PlayerVotingSystem class

---
+++ Assets/Scripts/Characters/PlayerVotingSystem.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerVotingSystem : MonoBehaviour
+{
+    private GameObject[] characterButtonObjects;
+    private string saveKey = "SelectedCharacter";
+
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        characterButtonObjects = GameObject.FindGameObjectsWithTag("CharacterButton");
+
+        foreach(GameObject characterButton in characterButtonObjects)
+        {
+            Button button = characterButton.GetComponent<Button>();
+
+            if(button != null)
+            {
+                string buttonName = characterButton.name;
+
+                button.onClick.AddListener(() => OnCharacterButtonClick(buttonName));
+            }
+        }
+
+        Button voteButton = GameObject.FindGameObjectWithTag("VoteButton").GetComponent<Button>();
+        voteButton.onClick.AddListener(() => OnVoteButtonClick());
+    }
+
+    public void OnCharacterButtonClick(string selectedButttonName)
+    {
+        PlayerPrefs.SetString(saveKey, selectedButttonName);
+        PlayerPrefs.Save();
+
+        Debug.Log($"Saved choice across scenes: Button {selectedButttonName}");
+    }
+
+    public void OnVoteButtonClick()
+    {
+        string characterName = PlayerPrefs.GetString(saveKey);
+        List<BaseCharacter> alivePlayers = GameManager.Instance.gameStateModel.GetAlivePlayers();
+
+        BaseCharacter target = alivePlayers.Find(p => p.gameObject.name == characterName);
+
+        if(target ==null)
+        {
+            Debug.Log("Target is null");
+        }
+
+        if (target != null)
+        {
+            Debug.Log("Target is not null");
+            playerController.OnVoteCast(target);
+        }
+        else
+        {
+            Debug.LogError($"Could not find an alive BaseCharacter component named {characterName}!");
+        }
+    }
+}
