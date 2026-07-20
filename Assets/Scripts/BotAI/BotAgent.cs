f26f6bb41ac59929f5efaea0c52419557b5f5a16
kristina-99 <kristina_atan@abv.bg>
2026-07-20 10:09:42 +0300

    * created VotingSystem for the bot * moved out voting logic from BotAgent to VotingSystem * As Bot Agent will be used for the states of the bot decided to separate the logic for cleaner code

--- Assets/Scripts/BotAI/BotAgent.cs
+++ Assets/Scripts/BotAI/BotAgent.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using Unity.VisualScripting;
 
 public class BotAgent : BaseCharacter
 {
@@ -14,50 +11,19 @@ public class BotAgent : BaseCharacter
             ExecuteNextCommand();
         }
     }
+
     public override void OnRoleAction()
     {
         throw new System.NotImplementedException();
     }
 
-    void OnEnable()
-    {
-        GameEvents.OnDayStarted += VoteRandomly;
-    }
-    void OnDisable()
-    {
-        GameEvents.OnDayStarted -= VoteRandomly;
-    }
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
-    }
-
-    private void VoteRandomly()
-    {
-
-    if (this == null)
-    {
-            return;
-    }
-
-    BaseCharacter votedCharacter = null;
-    float votingProbability = 2f / 3f;
-
-    if (UnityEngine.Random.value <= votingProbability)
-    {
-        var alivePlayers = GameManager.Instance.gameStateModel.GetAlivePlayers();
-        votedCharacter = alivePlayers[UnityEngine.Random.Range(0, alivePlayers.Count)];
-    }
-
-    string targetName = votedCharacter != null ? votedCharacter.gameObject.name : "Skip/No one";
-    UnityEngine.Debug.Log($"{gameObject.name} voted for: {targetName}");
-
-    ScheduleCommand(new VoteCommand(this, votedCharacter));
         
     }
 
